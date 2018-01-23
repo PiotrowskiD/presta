da7582b2574e309c0a35c51917e15dbc4c4002a2
zofiabanach <zuosia.b@gmail.com>
2018-01-23 12:31:30 +0100

    sa

--- www/html/modules/homefeatured/homefeatured.php
+++ www/html/modules/homefeatured/homefeatured.php
@@ -175,7 +175,7 @@ class HomeFeatured extends Module
 				  $ids .= $value['itemID'].",";
 				}*/
 				echo ' ok ';
-
+				echo 'SELECT * FROM '._DB_PREFIX_.'product WHERE id_product IN ('. $tabJson[0] .')';
 				$recommendedProducts = Db::getInstance()->executeS('SELECT * FROM '._DB_PREFIX_.'product WHERE id_product IN ('. $tabJson[0] .')');
 
 						$recommendedProducts2[0] = (array)(new Product($tabJson[0], false, '1'));
