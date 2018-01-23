bf3812b96078a9f4d4c30278501eaef453adc7a6
zofiabanach <zuosia.b@gmail.com>
2018-01-23 12:26:55 +0100

    json decode

--- www/html/modules/homefeatured/homefeatured.php
+++ www/html/modules/homefeatured/homefeatured.php
@@ -170,19 +170,19 @@ class HomeFeatured extends Module
 
 			echo (int)$tabJson[0];
 
-			if(count($content) == 0){
+			if(count($tabJson) > 0){
 				/*foreach ($recommendationsArray as $key => $value) {
 				  $ids .= $value['itemID'].",";
 				}*/
 
 
-				$recommendedProducts = Db::getInstance()->executeS('SELECT * FROM '._DB_PREFIX_.'product WHERE id_product IN ('.(int)$content[0].')');
+				$recommendedProducts = Db::getInstance()->executeS('SELECT * FROM '._DB_PREFIX_.'product WHERE id_product IN ('.(int)$tabJson[0].')');
 
-						$recommendedProducts2[0] = (array)(new Product($content[0], false, '1'));
+						$recommendedProducts2[0] = (array)(new Product($tabJson[0], false, '1'));
 						$recommendedProducts2[0]['price_without_reduction'] = '';
-						$recommendedProducts2[0]['id_image'] = Product::getCover((int)$content[0])['id_image'];
+						$recommendedProducts2[0]['id_image'] = Product::getCover((int)$tabJson[0])['id_image'];
 
-						$recommendedProducts2[0]['link'] = Context::getContext()->link->getProductLink((int)$content[0],
+						$recommendedProducts2[0]['link'] = Context::getContext()->link->getProductLink((int)$tabJson[0],
 						$recommendedProducts2[0]['link_rewrite'], $recommendedProducts2[0]['category'],
 						$recommendedProducts2[0]['ean13']);
 
